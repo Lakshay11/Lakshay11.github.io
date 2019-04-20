49997109c203e21d0cf6d2b45640220be969194f
itslakshay <lakshaykumar11@rediffmail.com>
2019-04-21 02:21:49 +0530

    added code

--- test/js/main.js
+++ test/js/main.js
@@ -2,3 +2,15 @@ $.getJSON("https://dev-testamp.pantheonsite.io/rest/data", function(data) {
   console.log(data.title);
   $(".wrapper").append('<h1>' + data.title + '</h1>');
 });
+
+const invocation = new XMLHttpRequest();
+const url = 'https://dev-testamp.pantheonsite.io/rest/data';
+
+function callOtherDomain(){
+  if(invocation) {
+    invocation.open('GET', url, true);
+    invocation.withCredentials = true;
+    invocation.onreadystatechange = handler;
+    invocation.send();
+  }
+}
