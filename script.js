db3ad492ec0a4b80c1d42866599cfdadcb8b5df4
Lakshay <kumarlakshay11@gmail.com>
2024-06-11 00:20:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,17 +26,33 @@ takePhotoButton.addEventListener('click', () => {
 });
 
 function uploadToCloudinary(imageDataUrl) {
-    const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dtjohf0uv/image/upload';
-    const CLOUDINARY_UPLOAD_PRESET = 'ml_default';
+    const blob = dataURLToBlob(imageDataUrl);
+    const formData = new FormData();
+    formData.append('file', blob);
+    formData.append('upload_preset', 'ml_default');
+    formData.append("tags", "browser_upload"); // Optional - add tag for image admin in Cloudinary
+    formData.append("file", file);
+    const config = {
+      headers: { "X-Requested-With": "XMLHttpRequest" },
+    };
 
-    axios.post(CLOUDINARY_URL, {
-        file: imageDataUrl,
-        upload_preset: CLOUDINARY_UPLOAD_PRESET
-    })
+    axios.post(`https://api.cloudinary.com/v1_1/dtjohf0uv/image/upload`, formData)
     .then(response => {
         console.log('Image uploaded successfully:', response.data);
     })
     .catch(error => {
         console.error('Error uploading image:', error);
     });
+}
+
+function dataURLToBlob(dataURL) {
+    const parts = dataURL.split(',');
+    const mime = parts[0].match(/:(.*?);/)[1];
+    const bstr = atob(parts[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
 }
